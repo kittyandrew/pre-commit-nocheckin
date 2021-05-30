24d415b0769fea6f6d3f2f1ca6b810cdf4aad215
kittyandrew <kitty@maudrew.dev>
2021-05-30 19:39:45 +0300

    better hook (still not working)

--- look.sh
+++ look.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-red=`tput setaf 1`
-reset=`tput sgr0`
-
-err_code=0
-shopt -s lastpipe
-grep -Rni '@nocheckin' $@ | while read -r line; do
-    echo -e "${red}$line${reset}"
-    err_code=1
-done
-
-exit $err_code
+# shopt -s lastpipe
+git diff --cached --name-only | \
+ GREP_COLOR='4;5;37;41' xargs grep --color --with-filename -ni '@nocheckin' >&2 \
+ && exit 1
